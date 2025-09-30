3df68017433b7d96d8523ff096142c719050bf3d
Luigi9665 <ventriglia.luigi@hotmail.com>
2025-09-30 17:18:07 +0200

    fix State

--- src/Components/AllTheBooks.jsx
+++ src/Components/AllTheBooks.jsx
@@ -12,17 +12,9 @@ import scifi from "../data/scifi.json";
 import { Component } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
-const books = {
-  fantasy,
-  history,
-  horror,
-  romance,
-  scifi,
-};
-
 class AllTheBooks extends Component {
   state = {
-    selectedGeners: "fantasy",
+    selectedGeners: fantasy,
   };
 
   render() {
@@ -32,35 +24,35 @@ class AllTheBooks extends Component {
           <Button
             variant="outline-light"
             className="text-black bg-gradient border-none fs-3 px-3 py-2"
-            onClick={() => this.setState({ selectedGeners: "fantasy" })}
+            onClick={() => this.setState({ selectedGeners: fantasy })}
           >
             Fantasy
           </Button>
           <Button
             variant="outline-light"
             className="text-black bg-gradient border-none fs-3 px-3 py-2"
-            onClick={() => this.setState({ selectedGeners: "history" })}
+            onClick={() => this.setState({ selectedGeners: history })}
           >
             History
           </Button>
           <Button
             variant="outline-light"
             className="text-black bg-gradient border-none fs-3 px-3 py-2"
-            onClick={() => this.setState({ selectedGeners: "horror" })}
+            onClick={() => this.setState({ selectedGeners: horror })}
           >
             Horror
           </Button>
           <Button
             variant="outline-light"
             className="text-black bg-gradient border-none fs-3 px-3 py-2"
-            onClick={() => this.setState({ selectedGeners: "romance" })}
+            onClick={() => this.setState({ selectedGeners: romance })}
           >
             Romance
           </Button>
           <Button
             variant="outline-light"
             className="text-black bg-gradient border-none fs-3 px-3 py-2"
-            onClick={() => this.setState({ selectedGeners: "scifi" })}
+            onClick={() => this.setState({ selectedGeners: scifi })}
           >
             Scifi
           </Button>
@@ -98,7 +90,7 @@ class AllTheBooks extends Component {
 
         <Container fluid className="my-5">
           <Row className="g-4">
-            {books[this.state.selectedGeners].map((book) => (
+            {this.state.selectedGeners.map((book) => (
               <Col sm={6} md={4} lg={3} xl={2} key={book.asin}>
                 <Card className="h-100">
                   <Card.Img style={{ height: "200px", objectFit: "cover" }} variant="top" src={book.img} />
